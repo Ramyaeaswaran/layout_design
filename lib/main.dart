f5b2994ad6397de070d911bb86e7810fc45300ff
Ramyaeaswaran <ramyaeaswaran77@gmail.com>
2019-10-14 18:06:50 +0530

    fit_and_finish

--- lib/main.dart
+++ lib/main.dart
@@ -20,14 +20,15 @@ Widget imagedetails() {
 Widget title() {
   return Text(
     'Strawberry Pavlova',
-    style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+    style: TextStyle(
+        color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
     textAlign: TextAlign.center,
   );
 }
 
 Widget description() {
   return Text(
-    'Pavlova is a meringue-based dessert named after the russian ballerine AnnaPavlova.Pavlova features a crisp crust and soft,light inside,topped with fruits and whipped cream.',
+    'Pavlova is a meringue-based dessert named after the russian ballerine Anna Pavlova. Pavlova features a crisp crust and soft, light inside, topped with fruit and whipped cream.',
     style: TextStyle(color: Colors.black87),
     textAlign: TextAlign.center,
   );
@@ -59,106 +60,52 @@ class recipe extends StatelessWidget {
 class landscape extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          flex: 1,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                  //  margin: EdgeInsets.symmetric(vertical:2, horizontal:2),
-                  width: 200,
-                  child: title()),
-              Container(
-                width: 200,
-                child: Center(
-                  child: description(),
-                ),
-              ),
-              Wrap(
-                alignment: WrapAlignment.center,
+    return  Row(
+        children: <Widget>[
+          Expanded(flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Icon(Icons.star, color: Colors.black54),
-                      Icon(Icons.star, color: Colors.black54),
-                      Icon(Icons.star, color: Colors.black54),
-                      Icon(Icons.star, color: Colors.black54),
-                      Icon(Icons.star, color: Colors.black54),
-                      Container(
-                        child: Text(
-                          '170 Reviews',
-                          //overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
+                  title(),
+                  Center(
+                    child: description(),
+                  ),
+                      Wrap(
+                        //alignment: WrapAlignment.center,
+                        alignment: WrapAlignment.center,
+                       // spacing: 20,
+                        children: <Widget>[
+                          stars(),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top:8.0),
+                            child: Text('170 Reviews',
+                            ),
+                          ),
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 2, horizontal: 2),
+                    width: 200,
+                    child: Reuseable(),
+                  ),
                 ],
               ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 2, horizontal: 2),
-                width: 200,
-                child:Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Icon(
-                          Icons.check_circle_outline,
-                          color: Colors.green,
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text('PREP:'),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text('25 min'),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Icon(Icons.timer, color: Colors.green),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text('COOK:'),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text('1 hour'),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Icon(Icons.person, color: Colors.green),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text('FEED:'),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text('4-6'),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
-        Expanded(
-          flex: 2,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Expanded(child: imagedetails()),
-            ],
+          Expanded(flex: 2,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Expanded(child: imagedetails()),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
     );
   }
 }
@@ -173,88 +120,107 @@ class portrait extends StatelessWidget {
           child: Container(child: imagedetails()),
         ),
         Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                  //  margin: EdgeInsets.symmetric(vertical:2, horizontal:2),
-                  width: 200,
-                  child: title()),
-              Container(
-                width: 200,
-                child: description(),
-              ),
-              Wrap(
-                direction: Axis.vertical,
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Icon(Icons.star, color: Colors.black54),
-                      Icon(Icons.star, color: Colors.black54),
-                      Icon(Icons.star, color: Colors.black54),
-                      Icon(Icons.star, color: Colors.black54),
-                      Icon(Icons.star, color: Colors.black54),
-                      Text(
-                        '170 Reviews',
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Icon(
-                        Icons.check_circle_outline,
-                        color: Colors.green,
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text('PREP:'),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text('25 min'),
-                    ],
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Icon(Icons.timer, color: Colors.green),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text('COOK:'),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text('1 hour'),
-                    ],
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Icon(Icons.person, color: Colors.green),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text('FEED:'),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text('4-6'),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                title(),
+                description(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Wrap(
+                              crossAxisAlignment: WrapCrossAlignment.center,
+                              children: <Widget>[
+                                stars(),
+                                SizedBox(
+                                  width: 5,
+                                ),
+                                Text(
+                                  '170 Reviews',
+                ),
+                      ],
+                    ),
+                          ],
+                        ),
+                Reuseable(),
+              ],
+            ),
           ),
         )
+      ],
+    );
+  }
+}
+
+class stars extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(Icons.star, color: Colors.black54),
+        Icon(Icons.star, color: Colors.black54),
+        Icon(Icons.star, color: Colors.black54),
+        Icon(Icons.star, color: Colors.black54),
+        Icon(Icons.star, color: Colors.black54),
+      ],
+    );
+  }
+}
+
+class Reuseable extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Icon(
+              Icons.check_circle_outline,
+              color: Colors.green,
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text('PREP:'),
+            SizedBox(
+              height: 5,
+            ),
+            Text('25 min'),
+          ],
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Icon(Icons.timer, color: Colors.green),
+            SizedBox(
+              height: 5,
+            ),
+            Text('COOK:'),
+            SizedBox(
+              height: 5,
+            ),
+            Text('1 hour'),
+          ],
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Icon(Icons.person_outline, color: Colors.green),
+            SizedBox(
+              height: 5,
+            ),
+            Text('FEED:'),
+            SizedBox(
+              height: 5,
+            ),
+            Text('4 - 6'),
+          ],
+        ),
       ],
     );
   }
